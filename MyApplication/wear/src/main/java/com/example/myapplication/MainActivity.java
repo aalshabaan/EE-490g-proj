fbe01afd0cb87a912b75d0efcfd9da84ea1b2e4c
Ahmedounet <56503724+Ahmedounet@users.noreply.github.com>
2020-11-30 19:06:13 +0100

    Corrected project dependencies

---
+++ MyApplication/wear/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.widget.TextView;
+
+public class MainActivity extends WearableActivity {
+
+    private TextView mTextView;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text);
+
+        //mTextView.setText("Hello round World");
+
+        //TextView textView=(R.id.w_display_message);
+
+        // Enables Always-on
+        setAmbientEnabled();
+    }
+}
