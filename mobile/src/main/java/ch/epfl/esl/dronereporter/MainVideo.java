582714fbb4b8e9bbbedb65fcc7b17b69abaa54d4
AbedShabaan <abed.shabaan@epfl.ch>
2021-01-14 18:15:29 +0100

    Removed the Mariana trench that hid onActivityResult

--- mobile/src/main/java/ch/epfl/esl/dronereporter/MainVideo.java
+++ mobile/src/main/java/ch/epfl/esl/dronereporter/MainVideo.java
@@ -172,15 +172,6 @@ public class MainVideo extends AppCompatActivity {
     }
 
 
-
-
-
-
-
-
-
-
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
